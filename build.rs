de19b7f81f9e6b0a76faa672a869686209659f44
ahaoboy <504595380@qq.com>
2024-07-02 23:50:55 +0800

    ignore error

--- build.rs
+++ build.rs
@@ -18,9 +18,9 @@ extern crate winres;
 
 #[cfg(windows)]
 fn main() {
-  if cfg!(target_os = "windows") {
-    let mut res = winres::WindowsResource::new();
-    res.set_icon("./assets/ico/app.ico");
-    res.compile().unwrap();
-  }
+    if cfg!(target_os = "windows") {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("./assets/ico/app.ico");
+        res.compile().unwrap();
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use anyhow::Result;
 
 use std::collections::VecDeque;
 
@@ -34,7 +35,7 @@ fn get_fps(cpu: f32) -> u64 {
     30
 }
 
-fn main() {
+fn start() -> Result<()> {
     // Since winit doesn't use gtk on Linux, and we need gtk for
     // the tray icon to show up, we need to spawn a thread
     // where we initialize gtk and create the tray_icon
@@ -79,16 +80,16 @@ fn main() {
 
     tray_menu
         .append_items(&[&dark_item, &light_item, &quit_item])
-        .unwrap();
+        .expect("tray_menu append_items error");
 
     let tray_icon = TrayIconBuilder::new()
         .with_menu(Box::new(tray_menu))
         .with_tooltip("run cat~")
         .with_icon(app_icon.clone())
         .build()
-        .unwrap();
+        .expect("tray_icon error");
 
-    let event_loop = EventLoop::new().unwrap();
+    let event_loop = EventLoop::new()?;
     let menu_channel = MenuEvent::receiver();
 
     let mut sys = System::new();
@@ -104,59 +105,77 @@ fn main() {
 
     let cpu_sample_gap = 250;
 
-    event_loop
-        .run(move |_, event_loop| {
-            // We add delay of 16 ms (60fps) to event_loop to reduce cpu load.
-            // This can be removed to allow ControlFlow::Poll to poll on each cpu cycle
-            // Alternatively, you can set ControlFlow::Wait or use TrayIconEvent::set_event_handler,
-            // see https://github.com/tauri-apps/tray-icon/issues/83#issuecomment-1697773065
-            event_loop.set_control_flow(ControlFlow::WaitUntil(
-                std::time::Instant::now() + std::time::Duration::from_millis(16),
-            ));
+    event_loop.run(move |_, event_loop| {
+        // We add delay of 16 ms (60fps) to event_loop to reduce cpu load.
+        // This can be removed to allow ControlFlow::Poll to poll on each cpu cycle
+        // Alternatively, you can set ControlFlow::Wait or use TrayIconEvent::set_event_handler,
+        // see https://github.com/tauri-apps/tray-icon/issues/83#issuecomment-1697773065
+        event_loop.set_control_flow(ControlFlow::WaitUntil(
+            std::time::Instant::now() + std::time::Duration::from_millis(16),
+        ));
 
-            if let Ok(event) = menu_channel.try_recv() {
-                if event.id == quit_item.id() {
-                    std::process::exit(0);
-                }
-                if event.id == dark_item.id() {
-                    theme = "dark";
-                }
-                if event.id == light_item.id() {
-                    theme = "light";
-                }
+        if let Ok(event) = menu_channel.try_recv() {
+            if event.id == quit_item.id() {
+                std::process::exit(0);
             }
+            if event.id == dark_item.id() {
+                theme = "dark";
+            }
+            if event.id == light_item.id() {
+                theme = "light";
+            }
+        }
 
-            let now = std::time::Instant::now();
+        let now = std::time::Instant::now();
 
-            sys.refresh_cpu();
-            let avg = if last_refresh_cpu.elapsed().as_millis() < cpu_sample_gap {
-                cpu_usage_stack.iter().sum::<f32>() / cpu_usage_stack.len() as f32
-            } else {
-                last_refresh_cpu = now;
-                let avg =
-                    sys.cpus().iter().map(|i| i.cpu_usage()).sum::<f32>() / sys.cpus().len() as f32;
-                cpu_usage_stack.push_back(avg);
+        sys.refresh_cpu();
+        let avg = if last_refresh_cpu.elapsed().as_millis() < cpu_sample_gap {
+            cpu_usage_stack.iter().sum::<f32>() / cpu_usage_stack.len() as f32
+        } else {
+            last_refresh_cpu = now;
+            let avg =
+                sys.cpus().iter().map(|i| i.cpu_usage()).sum::<f32>() / sys.cpus().len() as f32;
+            cpu_usage_stack.push_back(avg);
 
-                if cpu_usage_stack.len() > CPU_USAGE_STACK_SIZE {
-                    cpu_usage_stack.pop_front();
-                }
-                cpu_usage_stack.iter().sum::<f32>() / cpu_usage_stack.len() as f32
+            if cpu_usage_stack.len() > CPU_USAGE_STACK_SIZE {
+                cpu_usage_stack.pop_front();
+            }
+            cpu_usage_stack.iter().sum::<f32>() / cpu_usage_stack.len() as f32
+        };
+
+        let fps: u64 = get_fps(avg);
+        let ms = 1000 / fps;
+
+        if last_update_ts.elapsed().as_millis() >= ms.into() {
+            last_update_ts = now;
+            let list = match theme {
+                "dark" => &dark_cat_icons,
+                "light" => &light_cat_icons,
+                _ => &dark_cat_icons,
             };
+            let icon = &list[c % list.len()];
+            c = (c + 1) % list.len();
 
-            let fps: u64 = get_fps(avg);
-            let ms = 1000 / fps;
-
-            if last_update_ts.elapsed().as_millis() >= ms.into() {
-                last_update_ts = now;
-                let list = match theme {
-                    "dark" => &dark_cat_icons,
-                    "light" => &light_cat_icons,
-                    _ => &dark_cat_icons,
-                };
-                let icon = &list[c % dark_cat_icons.len()];
-                c = (c + 1) % list.len();
-                tray_icon.set_icon(Some(icon.clone())).unwrap();
+            match tray_icon.set_icon(Some(icon.clone())) {
+                Ok(_) => {}
+                Err(e) => {
+                    println!("{:?}", e)
+                }
             }
-        })
-        .unwrap();
+        }
+    })?;
+
+    Ok(())
+}
+
+fn main() {
+    loop {
+        let e = start();
+        match e {
+            Ok(_) => {}
+            Err(e) => {
+                println!("{:?}", e)
+            }
+        }
+    }
 }
